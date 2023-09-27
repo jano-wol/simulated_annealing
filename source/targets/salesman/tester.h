c2051255d0d86ba59ebc10c977d6c66990ada9ff
Andrzej Polak <jjjkamu@gmail.com>
2023-09-27 15:50:15 +0200

    Steps toward annealing test

--- source/targets/salesman/tester.h
+++ source/targets/salesman/tester.h
@@ -3,6 +3,26 @@
 
 #include "salesman_position.h"
 
-std::vector<std::shared_ptr<IPosition>> getStartPositions() { return {}; }
+std::vector<std::shared_ptr<IPosition>> getStartPositions(std::size_t numPositions, std::size_t numCities,
+                                                          double length)
+{
+  std::random_device rd;
+  auto mt = std::mt19937(rd);
+  auto dist = std::uniform_real_distribution<double>(0.0, 1.0);
+  std::vector<std::shared_ptr<IPosition>> ret;
+  ret.reserve(numPositions);
+  for (std::size_t idx = 0; idx < numPositions; ++idx) {
+    std::vector<std::pair<double, double>> cities;
+    cities.reserve(numCities);
+    for (std::size_t c = 0; c < numCities; ++c) {
+      double randomResult1 = dist(mt);
+      double randomResult2 = dist(mt);
+      cities.push_back({length * randomResult1, length * randomResult2});
+    }
+    auto startPosition = std::make_shared<SalesmanPosition>(cities);
+    ret.push_back(startPosition);
+  }
+  return ret;
+}
 
 #endif  // SIMULATED_ANNEALING_TARGETS_SALESMAN_TESTER_H_
