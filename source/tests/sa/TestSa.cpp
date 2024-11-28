33060ec107587d205336764ad1203994db08195f
Andrzej Polak <jjjkamu@gmail.com>
2024-11-28 19:31:38 +0100

    Finalize sa tests

--- source/tests/sa/TestSa.cpp
+++ source/tests/sa/TestSa.cpp
@@ -15,6 +15,8 @@ using namespace sa::sa;
 
 namespace
 {
+double delta = 1e-9;
+
 class DummyMove : public IMove
 {};
 
@@ -51,6 +53,39 @@ public:
   static std::size_t createNeighbourCounter;
 };
 
+class DummyFastPosition : public IPosition
+{
+public:
+  DummyFastPosition(double energy_) : energy(energy_) { ++created; }
+  DummyFastPosition(const DummySlowPosition& other) : energy(other.energy) { ++created; }
+  DummyFastPosition(DummySlowPosition&& other) noexcept : energy(other.energy) { ++created; }
+
+  std::shared_ptr<IMove> generateMove() const override { return std::make_shared<DummyMove>(); }
+  double getEnergy() const override
+  {
+    ++getEnergyCounter;
+    return energy;
+  }
+  std::optional<double> getDelta(const std::shared_ptr<IMove>& /*imove*/) const override
+  {
+    ++getDeltaCounter;
+    return -1;
+  }
+  void makeMove(const std::shared_ptr<IMove>& /*imove*/) override { ++makeMoveCounter; }
+  std::shared_ptr<IPosition> createNeighbour(const std::shared_ptr<IMove>& /*imove*/) const override
+  {
+    ++createNeighbourCounter;
+    auto ret = std::make_shared<DummySlowPosition>(energy - 1);
+    return ret;
+  }
+  double energy;
+  static std::size_t created;
+  static std::size_t getEnergyCounter;
+  static std::size_t getDeltaCounter;
+  static std::size_t makeMoveCounter;
+  static std::size_t createNeighbourCounter;
+};
+
 void nullStatics()
 {
   DummySlowPosition::created = 0;
@@ -58,6 +93,11 @@ void nullStatics()
   DummySlowPosition::getDeltaCounter = 0;
   DummySlowPosition::makeMoveCounter = 0;
   DummySlowPosition::createNeighbourCounter = 0;
+  DummyFastPosition::created = 0;
+  DummyFastPosition::getEnergyCounter = 0;
+  DummyFastPosition::getDeltaCounter = 0;
+  DummyFastPosition::makeMoveCounter = 0;
+  DummyFastPosition::createNeighbourCounter = 0;
 }
 
 std::size_t DummySlowPosition::created = 0;
@@ -65,6 +105,11 @@ std::size_t DummySlowPosition::getEnergyCounter = 0;
 std::size_t DummySlowPosition::getDeltaCounter = 0;
 std::size_t DummySlowPosition::makeMoveCounter = 0;
 std::size_t DummySlowPosition::createNeighbourCounter = 0;
+std::size_t DummyFastPosition::created = 0;
+std::size_t DummyFastPosition::getEnergyCounter = 0;
+std::size_t DummyFastPosition::getDeltaCounter = 0;
+std::size_t DummyFastPosition::makeMoveCounter = 0;
+std::size_t DummyFastPosition::createNeighbourCounter = 0;
 }  // namespace
 
 TEST(Sa, SlowAnnealing)
@@ -77,6 +122,20 @@ TEST(Sa, SlowAnnealing)
   EXPECT_EQ(DummySlowPosition::getDeltaCounter, 1000);
   EXPECT_EQ(DummySlowPosition::makeMoveCounter, 0);
   EXPECT_EQ(DummySlowPosition::createNeighbourCounter, 1000);
-  EXPECT_NEAR(sa.bestEnergy, -1000, 1e-9);
+  EXPECT_NEAR(sa.bestEnergy, -1000, delta);
+  nullStatics();
+}
+
+TEST(Sa, FastAnnealing)
+{
+  SA sa(Iteration(1000), Metropolis(), Linear());
+  std::shared_ptr<IPosition> position = std::make_shared<DummyFastPosition>(0);
+  sa.anneal(position);
+  EXPECT_EQ(DummyFastPosition::created, 1);
+  EXPECT_EQ(DummyFastPosition::getEnergyCounter, 1);
+  EXPECT_EQ(DummyFastPosition::getDeltaCounter, 1000);
+  EXPECT_EQ(DummyFastPosition::makeMoveCounter, 1000);
+  EXPECT_EQ(DummyFastPosition::createNeighbourCounter, 0);
+  EXPECT_NEAR(sa.bestEnergy, -1000, delta);
   nullStatics();
 }
