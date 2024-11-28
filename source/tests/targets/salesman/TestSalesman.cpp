6dd51c5908b492cb47c019fc660c0b086934d5f0
Andrzej Polak <jjjkamu@gmail.com>
2024-11-28 10:05:46 +0100

    Improve energy tests

--- source/tests/targets/salesman/TestSalesman.cpp
+++ source/tests/targets/salesman/TestSalesman.cpp
@@ -1,5 +1,7 @@
 #include <gmock/gmock.h>
 
+#include <cmath>
+
 #include <salesman/Position.h>
 
 using namespace sa::core;
@@ -21,10 +23,12 @@ TEST(Salersman, Energy)
   std::vector<std::pair<double, double>> cities1 = {{1, 1}};
   std::vector<std::pair<double, double>> cities2 = {{0, 0}, {0, 1}};
   std::vector<std::pair<double, double>> cities3 = {{0, 0}, {0, 3}, {4, 0}};
+  std::vector<std::pair<double, double>> cities3_2 = {{0, 0}, {0, 1}, {1, 0}};
   std::vector<std::pair<double, double>> cities4 = {{0, 0}, {0, 1}, {1, 1}, {1, 0}};
   testEnergy(cities0, 0);
   testEnergy(cities1, 0);
   testEnergy(cities2, 2);
   testEnergy(cities3, 12);
+  testEnergy(cities3_2, 2 + std::sqrt(2));
   testEnergy(cities4, 4);
 }
