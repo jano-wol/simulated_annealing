af33a7ef67b8244a665b6310e0921d43294fe31b
Andrzej Polak <jjjkamu@gmail.com>
2024-11-28 10:03:57 +0100

    improve tests

--- source/tests/targets/salesman/TestSalesman.cpp
+++ source/tests/targets/salesman/TestSalesman.cpp
@@ -5,10 +5,26 @@
 using namespace sa::core;
 using namespace sa::targets::salesman;
 
+namespace
+{
+void testEnergy(std::vector<std::pair<double, double>> cities, double expected)
+{
+  SalesmanPosition p(cities);
+  double energy = p.getEnergy();
+  EXPECT_NEAR(expected, energy, 1e-9);
+}
+}  // namespace
+
 TEST(Salersman, Energy)
 {
-  std::vector<std::pair<double, double>> cities_ = {{0, 0}, {0, 1}, {1, 1}, {1, 0}};
-  SalesmanPosition p(cities_);
-  double energy = p.getEnergy();
-  EXPECT_NEAR(4.0, energy, 1e-9);
+  std::vector<std::pair<double, double>> cities0;
+  std::vector<std::pair<double, double>> cities1 = {{1, 1}};
+  std::vector<std::pair<double, double>> cities2 = {{0, 0}, {0, 1}};
+  std::vector<std::pair<double, double>> cities3 = {{0, 0}, {0, 3}, {4, 0}};
+  std::vector<std::pair<double, double>> cities4 = {{0, 0}, {0, 1}, {1, 1}, {1, 0}};
+  testEnergy(cities0, 0);
+  testEnergy(cities1, 0);
+  testEnergy(cities2, 2);
+  testEnergy(cities3, 12);
+  testEnergy(cities4, 4);
 }
