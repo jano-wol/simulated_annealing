7f4d65f13e3f615f00eaa9d4bacd015fbb31aa07
janowol <jjjkamu@gmail.com>
2024-12-06 18:14:50 +0100

    Add Monitor

---
+++ source/lib/monitor/include/monitor/Monitor.h
@@ -0,0 +1,22 @@
+#ifndef SIMULATED_ANNEALING_MONITOR_MONITOR_H_
+#define SIMULATED_ANNEALING_MONITOR_MONITOR_H_
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <core/IMove.h>
+#include <core/IPosition.h>
+#include <core/MoveCandidate.h>
+
+namespace sa::monitor
+{
+class Monitor
+{
+public:
+  Monitor() {}
+};
+
+}  // namespace sa::monitor
+
+#endif  // SIMULATED_ANNEALING_MONITOR_MONITOR_H_
---
+++ source/lib/monitor/source/Monitor.cpp
@@ -0,0 +1,1 @@
+#include <monitor/Monitor.h>
