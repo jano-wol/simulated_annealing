458bebfc804b42ccdf7299c053166225d0e86afe
janowol <jjjkamu@gmail.com>
2024-12-08 09:29:32 +0100

    Refactor Random

--- source/lib/core/include/core/Random.h
+++ source/lib/core/include/core/Random.h
@@ -1,46 +1,20 @@
 #ifndef SIMULATED_ANNEALING_CORE_RANDOM_H_
 #define SIMULATED_ANNEALING_CORE_RANDOM_H_
 
-#include <chrono>
 #include <random>
-#include <thread>
 
 namespace sa::core
 {
 class Random
 {
 public:
-  static int randomInt(int min, int max)
-  {
-    std::uniform_int_distribution<int> dist(min, max);
-    return dist(getThreadLocalGenerator());
-  }
-
-  static double randomDouble(double min, double max)
-  {
-    std::uniform_real_distribution<double> dist(min, max);
-    return dist(getThreadLocalGenerator());
-  }
-
-  static void timeInit()
-  {
-    auto now = std::chrono::steady_clock::now();
-    auto time_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
-    getThreadLocalGenerator() = std::mt19937(static_cast<unsigned int>(time_since_epoch));
-  }
+  static int randomInt(int min, int max);
+  static double randomDouble(double min, double max);
+  static void timeInit();
 
 private:
-  static std::mt19937 createThreadLocalGenerator()
-  {
-    auto threadIdHash = std::hash<std::thread::id>()(std::this_thread::get_id());
-    return std::mt19937(static_cast<unsigned int>(threadIdHash));
-  }
-
-  static std::mt19937& getThreadLocalGenerator()
-  {
-    thread_local std::mt19937 generator = createThreadLocalGenerator();
-    return generator;
-  }
+  static std::mt19937 createThreadLocalGenerator();
+  static std::mt19937& getThreadLocalGenerator();
 };
 }  // namespace sa::core
 
--- source/lib/core/source/Random.cpp
+++ source/lib/core/source/Random.cpp
@@ -1,1 +1,37 @@
 #include <core/Random.h>
+
+#include <chrono>
+#include <thread>
+
+using namespace sa::core;
+
+int Random::randomInt(int min, int max)
+{
+  std::uniform_int_distribution<int> dist(min, max);
+  return dist(getThreadLocalGenerator());
+}
+
+double Random::randomDouble(double min, double max)
+{
+  std::uniform_real_distribution<double> dist(min, max);
+  return dist(getThreadLocalGenerator());
+}
+
+void Random::timeInit()
+{
+  auto now = std::chrono::steady_clock::now();
+  auto time_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+  getThreadLocalGenerator() = std::mt19937(static_cast<unsigned int>(time_since_epoch));
+}
+
+std::mt19937 Random::createThreadLocalGenerator()
+{
+  auto threadIdHash = std::hash<std::thread::id>()(std::this_thread::get_id());
+  return std::mt19937(static_cast<unsigned int>(threadIdHash));
+}
+
+std::mt19937& Random::getThreadLocalGenerator()
+{
+  thread_local std::mt19937 generator = createThreadLocalGenerator();
+  return generator;
+}
