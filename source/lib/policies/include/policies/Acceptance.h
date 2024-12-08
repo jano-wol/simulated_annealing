bb407941d575701e7330a2912757343b0eeb53dd
janowol <jjjkamu@gmail.com>
2024-12-08 10:32:51 +0100

    Refactor acceptance

--- source/lib/policies/include/policies/Acceptance.h
+++ source/lib/policies/include/policies/Acceptance.h
@@ -1,13 +1,8 @@
 #ifndef SIMULATED_ANNEALING_POLICIES_ACCEPTANCE_H_
 #define SIMULATED_ANNEALING_POLICIES_ACCEPTANCE_H_
 
-#include <algorithm>
-#include <cmath>
 #include <memory>
-#include <sstream>
 #include <string>
-
-#include <core/Random.h>
 
 namespace sa::policies
 {
@@ -25,24 +20,8 @@ class Metropolis final : public IAcceptance
 public:
   Metropolis(double normalizator_ = 1.0) : normalizator(normalizator_) {}
 
-  bool accept(double /*currEnergy*/, double delta, double temperature) const override
-  {
-    if (delta <= 0)
-      return true;
-    double threshold = std::exp(-delta / normalizator / temperature);
-    double randomResult = core::Random::randomDouble(0.0, 1.0);
-    return randomResult < threshold;
-  }
-
-  std::string toString() const override
-  {
-    std::stringstream ss;
-    ss << "Metropolis";
-    if (normalizator != 1) {
-      ss << "[norm=" << normalizator << "]";
-    }
-    return ss.str();
-  }
+  bool accept(double /*currEnergy*/, double delta, double temperature) const override;
+  std::string toString() const override;
 
   double normalizator;
 };
@@ -52,9 +31,8 @@ class Greedy final : public IAcceptance
 public:
   Greedy() {}
 
-  bool accept(double /*currEnergy*/, double delta, double /*temperature*/) const override { return delta <= 0; }
-
-  std::string toString() const override { return "Greedy"; }
+  bool accept(double /*currEnergy*/, double delta, double /*temperature*/) const override;
+  std::string toString() const override;
 };
 }  // namespace sa::policies
 
--- source/lib/policies/source/Acceptance.cpp
+++ source/lib/policies/source/Acceptance.cpp
@@ -1,1 +1,32 @@
 #include <policies/Acceptance.h>
+
+#include <cmath>
+#include <sstream>
+
+#include <core/Random.h>
+
+using namespace sa::core;
+using namespace sa::policies;
+
+bool Metropolis::accept(double /*currEnergy*/, double delta, double temperature) const
+{
+  if (delta <= 0)
+    return true;
+  double threshold = std::exp(-delta / normalizator / temperature);
+  double randomResult = Random::randomDouble(0.0, 1.0);
+  return randomResult < threshold;
+}
+
+std::string Metropolis::toString() const
+{
+  std::stringstream ss;
+  ss << "Metropolis";
+  if (normalizator != 1) {
+    ss << "[norm=" << normalizator << "]";
+  }
+  return ss.str();
+}
+
+bool Greedy::accept(double /*currEnergy*/, double delta, double /*temperature*/) const { return delta <= 0; }
+
+std::string Greedy::toString() const { return "Greedy"; }
