fc5b79f193df4e4bd6485755f375a95c434f8d75
Andrzej Polak <jjjkamu@gmail.com>
2024-12-12 13:34:31 +0100

    First steps toward reproducible testing

---
+++ source/lib/core/include/core/Io.h
@@ -0,0 +1,16 @@
+#ifndef SIMULATED_ANNEALING_CORE_IO_H_
+#define SIMULATED_ANNEALING_CORE_IO_H_
+
+#include <core/IPosition.h>
+
+namespace sa::core
+{
+class IGenerator
+{
+public:
+    virtual std::shared_ptr<core::IPosition> generatePosition(int idx) = 0;
+    virtual ~IGenerator() = default;
+};
+}  // namespace sa::core
+
+#endif  // SIMULATED_ANNEALING_CORE_IO_H_
--- source/lib/tester/source/ITester.cpp
+++
@@ -1,1 +0,0 @@
-#include <tester/ITester.h>
