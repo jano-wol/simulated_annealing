d413e978f5d072db594e9ae5c68292533f8af9ed
janowol <jjjkamu@gmail.com>
2024-12-16 00:41:45 +0100

    Steps to cleanup graphics

---
+++ source/bin/graphics/menubar.h
@@ -0,0 +1,40 @@
+#ifndef SIMULATED_ANNEALING_GRAPHICS_MENUBAR_H_
+#define SIMULATED_ANNEALING_GRAPHICS_MENUBAR_H_
+
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace imgui_menubar
+{
+struct file
+{
+  std::string alias;
+  std::filesystem::path path;
+};
+
+class file_browser_modal final
+{
+  const char* m_title;
+
+  bool m_oldVisibility;
+
+  int m_selection;
+
+  std::filesystem::path m_currentPath;
+  bool m_currentPathIsDir;
+
+  std::vector<file> m_filesInScope;
+
+public:
+  file_browser_modal(const char* title);
+
+  bool render(const bool isVisible, std::string& outPath);
+};
+
+void menuBarFile();
+
+
+};  // namespace imgui_menubar
+
+#endif  // SIMULATED_ANNEALING_GRAPHICS_MENUBAR_H_
