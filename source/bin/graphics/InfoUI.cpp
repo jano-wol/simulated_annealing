565269f254056ea19b006ae372a64401f613310d
janowol <jjjkamu@gmail.com>
2024-12-21 06:19:35 +0100

    Adjust popup window size

--- source/bin/graphics/InfoUI.cpp
+++ source/bin/graphics/InfoUI.cpp
@@ -6,11 +6,11 @@ void InfoUI::infoUpdate()
 {
   okPushed = false;
   ImGui::OpenPopup("Info");
-
+  ImGuiStyle& style = ImGui::GetStyle();
+  float size = (ImGui::CalcTextSize(message.c_str()).x + style.WindowPadding.x * 2.0f);
+  ImGui::SetNextWindowSizeConstraints(ImVec2(size, 0), ImVec2(size, FLT_MAX));
   if (ImGui::BeginPopupModal("Info", NULL, ImGuiWindowFlags_AlwaysAutoResize)) {
     ImGui::TextWrapped("%s", message.c_str());
-    ImGui::Separator();
-
     if (ImGui::Button("OK", ImVec2(120, 0))) {
       okPushed = true;
       ImGui::CloseCurrentPopup();
