349f047982c1f0ad610a606530eb6857f7deef9e
janowol <jjjkamu@gmail.com>
2024-12-22 08:24:49 +0100

    Add button

--- source/bin/graphics/SACallUI.cpp
+++ source/bin/graphics/SACallUI.cpp
@@ -4,7 +4,28 @@
 
 void SACallUI::saCallUpdate()
 {
-  if (ImGui::Button("Simulate")) {
+  ImVec2 cursorPos = ImGui::GetCursorScreenPos();
+  ImVec2 availSize = ImGui::GetContentRegionAvail();
+  float buttonWidth = availSize.x * 0.1f;
+  float buttonHeight = availSize.y * 0.05f;
+  ImVec2 buttonSize(buttonWidth, buttonHeight);
+  ImU32 buttonColor = IM_COL32(10, 120, 10, 255);
+  ImU32 buttonHoverColor = IM_COL32(20, 150, 20, 255);
+  ImU32 buttonActiveColor = IM_COL32(5, 90, 5, 255);
+  float rounding = 12.0f;
+  ImDrawList* drawList = ImGui::GetWindowDrawList();
+  bool isHovered = ImGui::IsItemHovered();
+  bool isActive = ImGui::IsItemActive();
+  ImVec2 buttonMin = cursorPos;
+  ImVec2 buttonMax = ImVec2(buttonMin.x + buttonWidth, buttonMin.y + buttonHeight);
+  ImGui::InvisibleButton("RoundedButton", buttonSize);
+  drawList->AddRectFilled(buttonMin, buttonMax,
+                          isActive ? buttonActiveColor : (isHovered ? buttonHoverColor : buttonColor), rounding);
+  ImVec2 textSize = ImGui::CalcTextSize("Simulate");
+  ImVec2 textPos =
+      ImVec2(buttonMin.x + (buttonWidth - textSize.x) * 0.5f, buttonMin.y + (buttonHeight - textSize.y) * 0.5f);
+  drawList->AddText(textPos, IM_COL32(255, 255, 255, 255), "Simulate");
+  if (ImGui::IsItemClicked()) {
     saCalled = true;
   }
 }
